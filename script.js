35734c15f21df194b78e35b7def996814242c22d
Anitha3650 <117801497+Anitha3650@users.noreply.github.com>
2023-05-05 19:41:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-let yourArray=[
-	[
-		{
-			'one':1;
-			'One':true;
-		}
-	],
-	[
-		{
-			'one':1;
-			'One':true;
-		}
-	],
-	[
-		{
-			'one':1;
-			'One':true;
-		}
-	],
-	[
-		{
-			'one':1;
-			'One':true;
-		}
-	],
-	[
-		{
-			'one':1;
-			'One':true;
-		}
-	]
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ],
+  [
+	{
+		bool1:true;
+		bool2:false;
+	}
+  ],
+  [
+	  {
+		  name:'Anitha'
+	  }
+  ],
+  [
+	  {
+		  age:22;
+	  }
+  ]
 ];
